e5a6b74be1e160e42e8be5fd1cfb3001df8324e4
gindia <omer.gindia91@gmail.com>
2021-06-17 17:52:40 +0200

    init

---
+++ xmonad.hs
@@ -0,0 +1,30 @@
+import XMonad
+import XMonad.Hooks.SetWMName
+
+-- XMobar hooks
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Actions.WorkspaceNames
+import XMonad.Util.Run(spawnPipe, hPutStrLn)
+
+main :: IO ()
+main = do
+    xmproc <- spawnPipe "xmobar ~/.xmonad/xmobarrc"
+    xmonad $ docks def
+        { modMask            = mod4Mask         -- superkey (WIN key)
+        , manageHook         = manageDocks <+> manageHook def
+        , layoutHook         = avoidStruts  $  layoutHook def
+
+        , handleEventHook    = handleEventHook def <+> docksEventHook
+
+        , terminal           = "alacritty"
+        , normalBorderColor  = "#000000"
+        , focusedBorderColor = "#3979C6"
+        , borderWidth        = 2
+        , startupHook        = setWMName "LG3D" -- for java applications
+        , logHook            = workspaceNamesPP xmobarPP
+            { ppOutput = hPutStrLn xmproc
+            , ppTitle = id
+            }
+            >>= dynamicLogWithPP
+        }
